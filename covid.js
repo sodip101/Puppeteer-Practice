fe919a82b0653ba80ebdcda31a6560776a66dea6
Sodip Bikram Thapa <sodip99@gmail.com>
2020-10-15 15:42:52 +0545

    added initial code for covid.js

---
+++ covid.js
@@ -0,0 +1,31 @@
+const puppeteer=require('puppeteer');
+const url='https://covid19.ekantipur.com/';
+
+const scraper=async()=>{
+    let browser=await puppeteer.launch();
+    let page=await browser.newPage();
+
+    await page.goto(url,{waitUntil:"networkidle0"});
+
+    let content=await page.evaluate(()=>{
+        let allDataArray=Array.from(document.querySelectorAll('table tbody tr td')).map(td=>td.innerText);;
+        let nepalIndex=allDataArray.indexOf("Nepal");
+        let result={
+            "Country": allDataArray[nepalIndex],
+            "Details": {
+                "Confirmed":allDataArray[nepalIndex+1],
+                "Deaths":allDataArray[nepalIndex+2],
+                "Recovered":allDataArray[nepalIndex+3]
+            }
+        };
+        return result;
+    });
+    debugger;
+    await browser.close();
+
+    return content;
+}
+
+scraper()
+.then(data=>console.log(data))
+.catch(err=>console.log(err));
